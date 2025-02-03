1fc35fadd8e813278036fd134d8edd6f5bff66d5
Monish Kumar B <107688632+m0nishkumar@users.noreply.github.com>
2025-02-03 18:41:54 +0530

    Update CpuIntensiveApp.java

--- CpuIntensiveApp.java
+++ CpuIntensiveApp.java
@@ -1,118 +1,51 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+	package main
 
-public class UnoptimizedCodeExample {
+	import (
+		"fmt"
+		"log"
+		"math"
+		"net/http"
+		_ "net/http/pprof" // Import pprof to enable profiling
+		"runtime"
+	)
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	func busyWork() {
+		for {
+			_ = math.Sqrt(float64(runtime.NumCPU())) * math.Pow(2.0, 10.0)
+		}
+	}
 
-        System.out.println("Enter a number for factorial:");
-        int num = scanner.nextInt();
-        System.out.println("Factorial of " + num + " is: " + calculateFactorial(num));
+	func moreBusyWork() {
+		for {
+			_ = math.Sin(float64(runtime.NumCPU())) * math.Cos(float64(runtime.NumCPU()))
+		}
+	}
 
-        System.out.println("\nEnter a number to check if it's prime:");
-        int primeCheck = scanner.nextInt();
-        System.out.println(primeCheck + " is prime? " + isPrime(primeCheck));
+	func evenMoreBusyWork() {
+		for {
+			_ = math.Log(float64(runtime.NumCPU())) * math.Exp(float64(runtime.NumCPU()))
+		}
+	}
 
-        System.out.println("\nEnter a string to reverse:");
-        String inputString = scanner.next();
-        System.out.println("Reversed string: " + reverseString(inputString));
+	func main() {
+		numCPU := runtime.NumCPU()
+		fmt.Printf("Starting infinite CPU-intensive tasks on %d cores...\n", numCPU)
 
-        System.out.println("\nEnter size of array:");
-        int size = scanner.nextInt();
-        int[] array = new int[size];
-        System.out.println("Enter " + size + " elements:");
-        for (int i = 0; i < size; i++) {
-            array[i] = scanner.nextInt();
-        }
-        System.out.println("Maximum element in the array: " + findMax(array));
+		// Set the maximum number of CPUs that can be executing simultaneously
+		runtime.GOMAXPROCS(numCPU)
 
-        scanner.close();
-    }
+		// Start HTTP server for pprof
+		go func() {
+			log.Println(http.ListenAndServe("localhost:6060", nil)) // Starts the pprof server on port 6060
+		}()
 
-    // Unoptimized factorial function
-    public static int calculateFactorial(int n) {
-        if (n == 0 || n == 1) return 1;
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            list.add(i);
-        }
+		// Run the busy work in separate goroutines
+		for i := 0; i < numCPU; i++ {
+			go busyWork()
+			go moreBusyWork()
+			go evenMoreBusyWork()
+		}
 
-        int result = 1;
-        for (int i = 0; i < list.size(); i++) {
-            result = result * list.get(i);
-        }
-
-        // Unnecessary check
-        if (result == factorialByRecursion(n)) {
-            return result;
-        } else {
-            return -1;
-        }
-    }
-
-    // Redundant recursive factorial for comparison
-    public static int factorialByRecursion(int n) {
-        if (n <= 1) return 1;
-        return n * factorialByRecursion(n - 1);
-    }
-
-    // Unoptimized prime checking
-    public static boolean isPrime(int num) {
-        if (num <= 1) return false;
-
-        ArrayList<Integer> divisors = new ArrayList<>();
-        for (int i = 1; i <= num; i++) {
-            if (num % i == 0) {
-                divisors.add(i);
-            }
-        }
-
-        // Prime check by size of divisors list
-        if (divisors.size() == 2) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Unoptimized string reversal
-    public static String reverseString(String str) {
-        char[] charArray = str.toCharArray();
-        ArrayList<Character> reversedList = new ArrayList<>();
-
-        for (int i = charArray.length - 1; i >= 0; i--) {
-            reversedList.add(charArray[i]);
-        }
-
-        String reversedString = "";
-        for (Character ch : reversedList) {
-            reversedString += ch;  // Inefficient string concatenation
-        }
-
-        return reversedString;
-    }
-
-    // Unoptimized maximum finder
-    public static int findMax(int[] arr) {
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < arr.length; i++) {
-            boolean isMax = true;
-
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] < arr[j]) {
-                    isMax = false;
-                    break;
-                }
-            }
-
-            if (isMax) {
-                max = arr[i];
-                break;
-            }
-        }
-
-        return max;
-    }
-}
+		// Block main goroutine to keep the program running
+		select {}
+	}
