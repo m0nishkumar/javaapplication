d6ed0deec819d447fe3a11a13778c5ca9c7108d4
Monish Kumar B <107688632+m0nishkumar@users.noreply.github.com>
2025-02-04 12:54:07 +0530

    Auto-update CpuIntensiveApp.java via GitHub API

--- CpuIntensiveApp.java
+++ CpuIntensiveApp.java
@@ -1,51 +1,65 @@
-	package main
+// Auto-updated via GitHub API
+```go
+package main
 
-	import (
-		"fmt"
-		"log"
-		"math"
-		"net/http"
-		_ "net/http/pprof" // Import pprof to enable profiling
-		"runtime"
-	)
+import (
+	"fmt"
+	"log"
+	"math"
+	"net/http"
+	_ "net/http/pprof" // Import pprof to enable profiling
+	"runtime"
+)
 
-	func busyWork() {
-		for {
-			_ = math.Sqrt(float64(runtime.NumCPU())) * math.Pow(2.0, 10.0)
+func busyWork() {
+	for {
+		x := math.Sqrt(float64(runtime.NumCPU())) * math.Pow(2.0, 10.0)
+		y := math.Sin(float64(runtime.NumCPU())) * math.Cos(float64(runtime.NumCPU()))
+		z := math.Log(float64(runtime.NumCPU())) * math.Exp(float64(runtime.NumCPU()))
+		if x != float64(x) || y != float64(y) || z != float64(z) {
+			return
 		}
 	}
+}
 
-	func moreBusyWork() {
-		for {
-			_ = math.Sin(float64(runtime.NumCPU())) * math.Cos(float64(runtime.NumCPU()))
+func moreBusyWork() {
+	for {
+		x := math.Sqrt(float64(runtime.NumCPU())) * math.Pow(2.0, 10.0)
+		y := math.Sin(float64(runtime.NumCPU())) * math.Cos(float64(runtime.NumCPU()))
+		z := math.Log(float64(runtime.NumCPU())) * math.Exp(float64(runtime.NumCPU()))
+		if x != float64(x) || y != float64(y) || z != float64(z) {
+			return
 		}
 	}
+}
 
-	func evenMoreBusyWork() {
-		for {
-			_ = math.Log(float64(runtime.NumCPU())) * math.Exp(float64(runtime.NumCPU()))
+func evenMoreBusyWork() {
+	for {
+		x := math.Sqrt(float64(runtime.NumCPU())) * math.Pow(2.0, 10.0)
+		y := math.Sin(float64(runtime.NumCPU())) * math.Cos(float64(runtime.NumCPU()))
+		z := math.Log(float64(runtime.NumCPU())) * math.Exp(float64(runtime.NumCPU()))
+		if x != float64(x) || y != float64(y) || z != float64(z) {
+			return
 		}
 	}
+}
 
-	func main() {
-		numCPU := runtime.NumCPU()
-		fmt.Printf("Starting infinite CPU-intensive tasks on %d cores...\n", numCPU)
+func main() {
+	numCPU := runtime.NumCPU()
+	fmt.Printf("Starting infinite CPU-intensive tasks on %d cores...\n", numCPU)
 
-		// Set the maximum number of CPUs that can be executing simultaneously
-		runtime.GOMAXPROCS(numCPU)
+	runtime.GOMAXPROCS(numCPU)
 
-		// Start HTTP server for pprof
-		go func() {
-			log.Println(http.ListenAndServe("localhost:6060", nil)) // Starts the pprof server on port 6060
-		}()
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil)) // Starts the pprof server on port 6060
+	}()
 
-		// Run the busy work in separate goroutines
-		for i := 0; i < numCPU; i++ {
-			go busyWork()
-			go moreBusyWork()
-			go evenMoreBusyWork()
-		}
-
-		// Block main goroutine to keep the program running
-		select {}
+	for i := 0; i < numCPU; i++ {
+		go busyWork()
+		go moreBusyWork()
+		go evenMoreBusyWork()
 	}
+
+	select {}
+}
+```
