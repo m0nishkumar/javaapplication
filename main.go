6b7a03aa380a54bf25e1438b949e16b5553f9ae3
Monish Kumar B <107688632+m0nishkumar@users.noreply.github.com>
2025-02-06 21:17:35 +0530

    Auto-update CpuIntensiveApp.java via GitHub API

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// Auto-updated via GitHub API
 package main
 
 import (
@@ -39,24 +40,18 @@ func main() {
 	numCPU := runtime.NumCPU()
 	fmt.Printf("Starting infinite CPU-intensive tasks on %d cores...\n", numCPU)
 
-	// Set the maximum number of CPUs that can be executing simultaneously
 	runtime.GOMAXPROCS(numCPU)
-
-	// Start HTTP server for pprof
 	go func() {
 		log.Println(http.ListenAndServe("localhost:6060", nil)) // Starts the pprof server on port 6060
 	}()
 
-	// Run the busy work in separate goroutines
 	for i := 0; i < numCPU; i++ {
 		go busyWork()
 		go moreBusyWork()
 		go evenMoreBusyWork()
 	}
 
-	// Example usage of factorial function (you can modify this to test with different inputs)
 	fmt.Println(factorial(10)) // Output: 3628800
 
-	// Block main goroutine to keep the program running
 	select {}
 }
